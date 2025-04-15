f37c9a0d0c3bdec9641db32deba323cf59964c0d
Eleni Houvarda <eleni.mariah04@gmail.com>
2025-04-15 14:56:41 -0400

    partial func for instruction menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject instructionButtonsGroup;
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+
+    private bool menuOpen = false;
+
+    public void ToggleMenu()
+    {
+        menuOpen = !menuOpen;
+        instructionButtonsGroup.SetActive(menuOpen);
+    }
+
+    public void TogglePanel(GameObject panel)
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+}
